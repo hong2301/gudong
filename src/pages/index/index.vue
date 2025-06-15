8ed4e69b5472f13f1d4eaec8b44bb2d52be032b9
lzf <honononong@icloud.com>
2025-06-15 16:59:26 +0800

    feat(): 大字事件挂载

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,7 +7,24 @@
         class="transition"
         :style="{ height: `${contentHeight / 5}px` }"
       ></view>
-      <view class="mnue" :style="{ height: `${contentHeight}px` }"> </view>
+      <view class="mnue" :style="{ height: `${contentHeight}px` }">
+        <view class="bigTextBox">
+          <view
+            class="bigText"
+            :style="{ transform: `scale(${yhScale}) ` }"
+            @tap="tapYh"
+            >烟火集</view
+          >
+          <view class="divider"></view>
+          <view
+            class="bigText"
+            :style="{ transform: `scale(${jwScale}) ` }"
+            @tap="tapJw"
+            >集味簿</view
+          >
+        </view>
+        <view class="divider1"></view>
+      </view>
     </view>
   </Layout>
 </template>
@@ -21,6 +38,12 @@ import { ref } from "vue";
 
 const bubuRef = ref();
 const contentHeight = ref(uni.getWindowInfo().safeArea.height / 2);
+const yhScale = ref(1);
+const jwScale = ref(1);
+
+/**
+ * 点击布布
+ */
 const tapBubu = () => {
   bubuRef?.value?.tapImg();
   bubuRef?.value?.startSay("你好呀", ["50%", "80%", "rgb(146,107,77)"], {
@@ -28,6 +51,25 @@ const tapBubu = () => {
     volume: 1,
   });
 };
+
+// 跳转烟火集
+const tapYh = () => {
+  yhScale.value = 1.1;
+  uni.vibrateShort();
+  setTimeout(() => {
+    yhScale.value = 1;
+  }, 100);
+};
+
+// 跳转集味簿
+const tapJw = () => {
+  jwScale.value = 1.1;
+  uni.vibrateShort();
+  setTimeout(() => {
+    jwScale.value = 1;
+  }, 100);
+};
+
 onLoad(() => {
   console.log("index Load");
 });
@@ -48,7 +90,7 @@ onHide(() => {
   flex-direction: column;
   align-items: center;
   justify-content: flex-end;
-  background-image: url("../../static/logo.png");
+  background-image: url("../../static/");
 }
 .transition {
   width: 100%;
@@ -71,5 +113,32 @@ onHide(() => {
   width: 100%;
   height: 200px;
   background-color: rgb(245, 241, 230);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.bigTextBox {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bigText {
+  font-size: 45rpx;
+  color: rgb(119, 112, 96);
+  transition: transform 0.3s ease;
+}
+.divider {
+  border: 1rpx solid rgb(119, 112, 96, 0.8);
+  border-radius: 100rpx;
+  height: 40%;
+  margin-inline: 15%;
+}
+.divider1 {
+  border-top: 1rpx solid rgb(119, 112, 96, 0.8);
+  border-radius: 200rpx;
+  width: 85%;
+  height: 0;
 }
 </style>
