1a5c11a4b84bd9f9931d2ab2a486fafe6698225b
lzf <honononong@icloud.com>
2025-06-15 15:45:19 +0800

    feat(): 布布

--- src/components/character/index.vue
+++ src/components/character/index.vue
@@ -1,14 +1,132 @@
 <template>
-  <view class="overture">
-    <u-image class="img" src="/static/蟑螂布.png"></u-image>
+  <view
+    class="overture"
+    :style="{ width: `${size}rpx`, transform: `scale(${imgScale}) ` }"
+  >
+    <view class="content">
+      <u-image
+        mode="widthFix"
+        width="100%"
+        :src="imgSrc"
+        :show-loading="true"
+        class="img"
+        @tap="tapImg"
+      ></u-image>
+    </view>
+    <view :style="sayOption" class="text-box">
+      {{ say }}
+    </view>
   </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { onLoad, onShow, onHide, onUnload } from "@dcloudio/uni-app";
+
+const props = defineProps({
+  size: {
+    type: Number,
+    required: true,
+    default: "100",
+  },
+  fontSize: {
+    type: Number,
+    required: true,
+    default: "45",
+  },
+  imgSrc: {
+    type: String,
+    default: "/static/布布/蟑螂布.png",
+  },
+});
+
+const sayOption = ref({
+  top: "0%",
+  left: "0%",
+  color: "rgb(146,107,77)",
+});
+const say = ref("");
+const imgScale = ref(1);
+
+/**
+ * 点击图片
+ */
+const tapImg = () => {
+  imgScale.value = 1.1;
+  setTimeout(() => {
+    imgScale.value = 1;
+  }, 100);
+};
+
+/**
+ * 开始讲话
+ */
+const startSay = (
+  text: string,
+  options: string[],
+  sound: { src: string; volume: number },
+  seed: number = 100
+) => {
+  say.value = "";
+  sayOption.value = JSON.parse(
+    JSON.stringify({
+      top: options[0],
+      left: options[1],
+      color: options[2],
+    })
+  );
+
+  const bgm = uni.createInnerAudioContext();
+  bgm.autoplay = true;
+  bgm.src = sound.src;
+  bgm.volume = sound.volume;
+  bgm.play();
+  const setIntervalData = setInterval(() => {
+    if (say.value.length != text.length) {
+      say.value += text[say.value.length];
+    } else {
+      clearInterval(setIntervalData);
+    }
+  }, seed);
+};
+
+defineExpose({
+  tapImg,
+  startSay,
+});
+
+onLoad(() => {
+  console.log("character Load");
+});
+onShow(() => {
+  console.log("character Show");
+});
+onHide(() => {
+  console.log("character Hide");
+});
+onUnload(() => {
+  console.log("character Hide");
+});
+</script>
 
 <style scoped>
 .overture {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  position: relative;
+  transition: transform 0.3s ease;
+}
+.content {
+  width: 100%;
+  display: block;
 }
 .img {
+}
+.text-box {
+  position: absolute;
+  width: 100%;
+  font-weight: 800;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,15 +2,37 @@
   <Layout>
     <view class="overture">
       <Head></Head>
-      <Bubu></Bubu>
+      <Bubu ref="bubuRef" :size="450" :font-size="45"></Bubu>
+      <button @tap="test">123</button>
     </view>
   </Layout>
 </template>
 
 <script setup lang="ts">
+import { onLoad, onShow, onHide, onReady } from "@dcloudio/uni-app";
 import Layout from "@/components/layouts/index.vue";
 import Bubu from "@/components/character/index.vue";
 import Head from "@/components/head/index.vue";
+import { ref } from "vue";
+
+const bubuRef = ref();
+
+const test = () => {
+  bubuRef?.value?.startSay("123", ["50%", "50%", "rgb(146,107,77)"], {
+    src: "/static/布布/声音/哒哒哒哒哒.m4a",
+    volume: 1,
+  });
+};
+onLoad(() => {
+  console.log("index Load");
+});
+onShow(() => {
+  console.log("index Show");
+});
+onReady(() => {});
+onHide(() => {
+  console.log("index Hide");
+});
 </script>
 
 <style scoped>
