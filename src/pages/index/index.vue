de08182b818e4d2c205d16c0b16bd1f3a9a0e7b3
lzf <honononong@icloud.com>
2025-06-21 22:13:33 +0800

    feat(): 登陆按钮挂载

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,29 @@
 <template>
   <Layout>
     <view class="overture">
+      <view
+        class="head"
+        :style="{
+          width: `${eleWidth1}px`,
+          marginBottom: `${capsuleRightInterval * 2}px`,
+        }"
+      >
+        <view
+          class="bubu-box"
+          :style="{
+            marginRight: `${capsuleRightInterval * 2}px`,
+          }"
+        ></view>
+        <view class="user-box" @tap="tapDl">
+          <view
+            class="item-img-box1"
+            :style="{ transform: `scale(${dlScale}) ` }"
+          >
+            <image class="item-img" src="/static/shouye.png" mode="widthFix" />
+          </view>
+          <view class="item-text" style="font-weight: 400">登陆</view>
+        </view>
+      </view>
       <view class="content" :style="{ width: `${eleWidth1}px` }">
         <view class="item-box" @tap="tapDc">
           <view
@@ -12,6 +35,7 @@
           <view class="item-text">烟火集</view>
           <view class="item-text1">看看想吃点什么</view>
         </view>
+        <view class="divider"></view>
         <view class="item-box" @tap="tapDd">
           <view
             class="item-img-box"
@@ -33,17 +57,35 @@ import Layout from "@/components/layouts/index.vue";
 import { ref } from "vue";
 
 // 胶囊右间隔
-const capsuleRightInterval =
+const capsuleRightInterval = ref<number>(
   uni.getWindowInfo().safeArea.right -
-  uni.getMenuButtonBoundingClientRect().right;
+    uni.getMenuButtonBoundingClientRect().right
+);
 // 屏幕宽度
 const areaWidth = uni.getWindowInfo().safeArea.width;
 // 元素宽度
-const eleWidth1 = ref<number>(areaWidth - 4 * capsuleRightInterval);
+const eleWidth1 = ref<number>(areaWidth - 4 * capsuleRightInterval.value);
 // 点餐图标的缩放
 const dcScale = ref<number>(1);
 // 订单图标的缩放
 const ddScale = ref<number>(1);
+// 登陆图标的缩放
+const dlScale = ref<number>(1);
+
+/**
+ * 点击登陆
+ */
+const tapDl = () => {
+  console.log("点击登陆");
+  dlScale.value = 1.1;
+  uni.vibrateShort();
+  setTimeout(() => {
+    dlScale.value = 1;
+    // uni.navigateTo({
+    //   url: "/login-package/pages/login/index",
+    // });
+  }, 100);
+};
 
 /**
  * 点击点餐
@@ -101,11 +143,52 @@ onHide(() => {
   justify-content: center;
   background-color: $bg-color;
 }
+.head {
+  display: flex;
+  justify-content: space-between;
+}
+.bubu-box {
+  height: 100%;
+  background-color: $ele-color;
+  border-radius: 15rpx;
+  display: flex;
+  flex: auto;
+}
+.user-box {
+  width: 30%;
+  background-color: $ele-color;
+  aspect-ratio: 1;
+  border-radius: 15rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.item-img-box1 {
+  width: 50%;
+  aspect-ratio: 1;
+  background-color: $icon-bg-color;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transition: transform 0.3s ease;
+}
 .content {
   border-radius: 15rpx;
   display: flex;
   background-color: $ele-color;
   padding-block: 50rpx;
+  position: relative;
+  align-items: center;
+  box-shadow: 0 4rpx 15rpx rgba(0, 0, 0, 0.08); /* 添加阴影 */
+}
+.divider {
+  position: absolute;
+  height: 50%;
+  width: 1rpx;
+  background-color: rgba(0, 0, 0, 0.1);
+  left: 50%;
 }
 .item-box {
   width: 50%;
@@ -131,6 +214,7 @@ onHide(() => {
   margin-top: 5%;
   font-size: 33rpx;
   font-weight: 800;
+  color: $font-color;
 }
 .item-text1 {
   margin-top: 1%;
