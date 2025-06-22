0bb158790056d33e0569afd69639a42227b4516d
lzf <honononong@icloud.com>
2025-06-22 13:08:56 +0800

    feat(): 安装pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import uviewPlus from 'uview-plus'
+import { createPinia } from 'pinia'
+import piniapluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 export function createApp() {
+  const pinia = createPinia();
+  pinia.use(piniapluginPersistedstate);
   const app = createSSRApp(App);
   app.use(uviewPlus)
+  app.use(pinia)
   return {
     app,
   };
