5b04521ab5d69a682c1eb5c889cdd140edca95ad
lzf <honononong@icloud.com>
2025-06-24 02:27:23 +0800

    feat(): 下降按钮

--- src/components/loginCard/index.vue
+++ src/components/loginCard/index.vue
@@ -6,6 +6,12 @@
       transform: `translateY(${mainBtn ? '0' : '100'}%)`,
     }"
   >
+    <view
+      class="bottom"
+      :style="{ opacity: `${mainBtn ? 100 : 0}%` }"
+      @tap="tapBottom"
+      >⟨</view
+    >
   </view>
 </template>
 
@@ -23,6 +29,11 @@ const mainBtn = ref(0);
 
 // 点击mask
 const tapMask = () => {
+  mainBtn.value = 0;
+  emit("update:btn", mainBtn.value);
+};
+// 点击关闭
+const tapBottom = () => {
   mainBtn.value = 0;
   emit("update:btn", mainBtn.value);
 };
@@ -51,10 +62,24 @@ watch(
   position: fixed;
   width: 100%;
   height: 60%;
-  background-color: red;
+  background-color: $bg-color2;
   bottom: 0%;
   z-index: 999;
   border-radius: 40rpx 40rpx 0 0;
   transition: transform 0.3s ease;
+}
+.bottom {
+  width: 100%;
+  height: 60rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 800;
+  font-size: 40rpx;
+  line-height: 60rpx;
+  transform: rotate(270deg);
+  position: absolute;
+  top: 0;
+  transition: opacity 3s ease;
 }
 </style>
