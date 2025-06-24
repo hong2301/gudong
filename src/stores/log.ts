6a9e4a967b5fa9c3ebafde0f1e9adcaa4bf7814f
lzf <honononong@icloud.com>
2025-06-24 17:45:02 +0800

    feat(): 删除测试图片

--- src/stores/log.ts
+++ src/stores/log.ts
@@ -6,36 +6,7 @@ export const useLogStore = defineStore(
     'log',
     () => {
         //// 响应式数据
-        const rows = ref<logType[]>([
-            {
-                id: "1",
-                orderId: "1",
-                time: "2025年6月15日",
-                describe: "鸡腿肉太加分了",
-                imgSrc: "/static/foods/荔枝鸡煲.jpeg",
-            },
-            {
-                id: "2",
-                orderId: "2",
-                time: "2025年6月14日",
-                describe: "牛肉有点腥味",
-                imgSrc: "/static/foods/菠萝牛肉粒.jpeg",
-            },
-            {
-                id: "3",
-                orderId: "3",
-                time: "2025年6月13日",
-                describe: "咖喱咖喱",
-                imgSrc: "/static/foods/咖喱鸡翅.jpeg",
-            },
-            {
-                id: "4",
-                orderId: "4",
-                time: "2025年6月12日",
-                describe: "挑虾线是一件很恐怖的事情",
-                imgSrc: "/static/foods/白灼大虾.jpeg",
-            },
-        ])
+        const rows = ref<logType[]>()
 
 
         //外部可访问
