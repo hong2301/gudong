b7b6a391f33e4b61bdcc4fe03ebe3910acec9bf5
lzf <honononong@icloud.com>
2025-06-24 18:50:50 +0800

    feat(): user函数

---
+++ uniCloud-aliyun/cloudfunctions/userAdd/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const db=uniCloud.database()
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	const result=await db.collection("user").add(event)
+	//返回数据给客户端
+	return result
+};
