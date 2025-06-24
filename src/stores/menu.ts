075fd35819adadec1f11d75fbe308bdfc739724a
lzf <honononong@icloud.com>
2025-06-24 20:35:19 +0800

    feat(): 菜单

--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -13,21 +13,6 @@ export const useMenuStore = defineStore(
                 isNew: true,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 1,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                        des: '好吃'
-                    },
-                    {
-                        dishId: 2,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
                 ],
             },
             {
@@ -37,20 +22,6 @@ export const useMenuStore = defineStore(
                 isNew: false,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 3,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 4,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
                 ],
             },
             {
@@ -60,252 +31,41 @@ export const useMenuStore = defineStore(
                 isNew: false,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 5,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 6,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
+
                 ],
             },
             {
                 tapId: 't4',
-                text: "海鲜",
-                imgSrc: "../../static/xia.png",
+                text: "汤",
+                imgSrc: "../../static/tang.png",
                 isNew: false,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 7,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 8,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
+
                 ],
             },
             {
                 tapId: 't5',
-                text: "招牌",
-                imgSrc: "../../static/a3.png",
-                isNew: true,
+                text: "海鲜",
+                imgSrc: "../../static/xia.png",
+                isNew: false,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 9,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 10,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
+
                 ],
             },
             {
                 tapId: 't6',
-                text: "素菜",
-                imgSrc: "../../static/yumi.png",
+                text: "烧烤",
+                imgSrc: "../../static/shaokao.png",
                 isNew: false,
                 order: 0,
                 dish: [
-                    {
-                        dishId: 11,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 12,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
+
                 ],
             },
-            {
-                tapId: 't7',
-                text: "荤菜",
-                imgSrc: "../../static/rou.png",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 13,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 14,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't8',
-                text: "海鲜",
-                imgSrc: "../../static/xia.png",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 15,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 16,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't9',
-                text: "招牌",
-                imgSrc: "../../static/a3.png",
-                isNew: true,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 17,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 18,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't10',
-                text: "素菜",
-                imgSrc: "../../static/yumi.png",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 19,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 20,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't11',
-                text: "荤菜",
-                imgSrc: "../../static/rou.png",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 21,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 22,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't12',
-                text: "海鲜",
-                imgSrc: "../../static/xia.png",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 23,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 24,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
-            {
-                tapId: 't13',
-                text: "",
-                imgSrc: "",
-                isNew: false,
-                order: 0,
-                dish: [
-                    {
-                        dishId: 25,
-                        name: "可乐鸡翅",
-                        num: "5",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                    {
-                        dishId: 26,
-                        name: "荔枝鸡煲",
-                        num: "1",
-                        imgSrc: "../../static/foods/荔枝鸡煲.jpeg",
-                        order: 0,
-                    },
-                ],
-            },
+
+
         ])
 
         // 清空选择
