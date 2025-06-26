43b296888d118b3045f713d4fc7d71296cfb7efd
lzf <honononong@icloud.com>
2025-06-26 12:46:34 +0800

    feat(): 背景音乐挂载

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { onLaunch, onShow, onHide, onReady } from "@dcloudio/uni-app";
+// 背景音乐
+const mainBgm = uni.createInnerAudioContext();
+
+// 背景音乐
+const bgmPlay = () => {
+  mainBgm.autoplay = true;
+  mainBgm.src =
+    "https://mp-eb96f56f-cca7-47e5-802c-7542fcfdfdb9.cdn.bspapp.com/末白镇_吉他.m4a";
+  mainBgm.loop = true;
+  mainBgm.play();
+  console.log("播放");
+};
 
 onLaunch(() => {
   console.log("App Launch");
 });
 onShow(() => {
   console.log("App Show");
+  bgmPlay();
 });
+
 onHide(() => {
   console.log("App Hide");
 });
