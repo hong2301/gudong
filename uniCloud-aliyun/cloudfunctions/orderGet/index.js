692488bec2396fbde34c0e67b145779c0626a8bf
lzf <honononong@icloud.com>
2025-06-26 17:19:54 +0800

    feat(): 获取订单的方法

---
+++ uniCloud-aliyun/cloudfunctions/orderGet/index.js
@@ -0,0 +1,15 @@
+'use strict';
+const db=uniCloud.database()
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	const { _id } = event;
+	const result = await db.collection("order")
+	.where({
+	  _id: db.RegExp({
+		regexp: _id
+	  })
+	})
+	.get();
+	//返回数据给客户端
+	return result.data
+};
