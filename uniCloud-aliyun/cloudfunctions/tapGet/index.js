5267acdca3c3bd8d6dad848f9b6ebf313e72384a
lzf <honononong@icloud.com>
2025-06-27 20:45:41 +0800

    refactor(): 菜单排序

--- uniCloud-aliyun/cloudfunctions/tapGet/index.js
+++ uniCloud-aliyun/cloudfunctions/tapGet/index.js
@@ -1,9 +1,12 @@
 'use strict';
-const db=uniCloud.database()
+const db = uniCloud.database();
+
 exports.main = async (event, context) => {
-	//event为客户端上传的参数
-	const result=await db.collection("tap").get()
-	
-	//返回数据给客户端
-	return result
+  //event为客户端上传的参数
+  const result = await db.collection("tap")
+    .orderBy("sort", "asc")  // 按照sort字段升序排列(从小到大)
+    .get();
+  
+  //返回数据给客户端
+  return result;
 };
