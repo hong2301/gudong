c9f11eb32b9ef9eb26a244e94b561e913d8fce27
lzf <honononong@icloud.com>
2025-07-07 03:32:05 +0800

    feat(): 吃饭次数

--- uniCloud-aliyun/cloudfunctions/orderOk/index.js
+++ uniCloud-aliyun/cloudfunctions/orderOk/index.js
@@ -5,6 +5,16 @@ exports.main = async (event, context) => {
 	const {_id,...data}=event.item
 	const result=await collection.doc(_id).update(data);
 	
+	// 用户次数
+	const order=await collection.doc(_id).get();
+	const {userId}=order.data[0]
+	const userData=await db.collection('user').doc(userId).get();
+	userData.data[0].eatNum++;
+	await db.collection('user').doc(userId).update({
+		eatNum:userData.data[0].eatNum
+	});
+	
+	// 食物次数
 	const dishCollection = db.collection('dish');
 	data.dish.forEach(dItem=>{
 		dItem.num++
@@ -15,6 +25,7 @@ exports.main = async (event, context) => {
 			  updateTime:data.updateTime,
 		});
 	})
+	
 	
 	return result
 };
