7a3250ef61dbf4e3c48b88136032ec5c445f05a0
疯了个疯 <honononong@icloud.com>
2025-07-26 23:44:40 +0800

    refactor(): 搞定接口调整

--- uniCloud-aliyun/cloudfunctions/orderOk/index.js
+++ uniCloud-aliyun/cloudfunctions/orderOk/index.js
@@ -1,26 +1,26 @@
 'use strict';
 const db = uniCloud.database()
 exports.main = async (event, context) => {
-	const collection = db.collection('order');
-	const { _id, ...data } = event.item
-	const result = await collection.doc(_id).update(data);
+    const collection = db.collection('order');
+    const { _id, ...data } = event.item
+    const result = await collection.doc(_id).update(data);
 
-	// 用户次数
-	const userData = await db.collection('user').doc(data.userId).get();
-	userData.data[0].eatNum = (userData.data[0].eatNum || 0) + 1
-	db.collection('user').doc(data.userId).update({
-		eatNum: userData.data[0].eatNum
-	});
+    // 用户次数
+    const userData = await db.collection('user').doc(data.userId).get();
+    userData.data[0].eatNum = (userData.data[0].eatNum || 0) + 1
+    await db.collection('user').doc(data.userId).update({
+        eatNum: userData.data[0].eatNum
+    });
 
-	// 食物次数
-	const dishCollection = db.collection('dish');
-	data.dish.forEach(dItem => {
-		dItem.num++
-		dItem.updateTime = Date.now()
-		const { _id, ...data } = dItem
-		dishCollection.doc(_id).update(data);
-	})
+    // 食物次数
+    const dishCollection = db.collection('dish');
+    // 使用 for...of 替代 forEach 以便使用 await
+    for (const dItem of data.dish) {
+        dItem.num++
+        dItem.updateTime = Date.now()
+        const { _id, ...updateData } = dItem
+        await dishCollection.doc(_id).update(updateData);
+    }
 
-
-	return result
+    return result
 };
